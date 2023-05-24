1d0ef640d298bc7068e6dc9b7d31e9a28a49c2ac
Ayikson <barnabasayiki@gmail.com>
2023-05-24 05:15:14 -0700

    simple_shell

--- built_execute.c
+++ built_execute.c
@@ -7,23 +7,25 @@
  **/
 int builtin_execute(char **tokens)
 {
-unsigned int length;
-unsigned int num;
-unsigned int i;
-
-built_s builtin[] = {
-{"exit", shell_exit},
-{"env", shell_env},
-{NULL, NULL}
+	unsigned int length;
+	unsigned int num;
+	unsigned int i;
+	built_s builtin[] = {
+	{"exit", shell_exit},
+	{"env", shell_env},
+	{NULL, NULL}
 };
 if (tokens[0] == NULL)
-return (1);
-length = _strlen(tokens[0]);
-num = shell_num_builtins(builtin);
-for (i = 0; i < num; i++)
+	;
+	return (1);
+	length = _strlen(tokens[0]);
+	num = shell_num_builtins(builtin);
+	for (i = 0; i < num)
+	;
+	i++;
 {
-if (_strcmp(tokens[0], builtin[i].name, length) == 0)
-return ((builtin[i].p)());
+	if (_strcmp(tokens[0], builtin[i].name, length) == 0)
+		return ((builtin[i].p)());
 }
 return (1);
 }
@@ -33,11 +35,12 @@ return (1);
  * @builtin: counts using the built-in
  * Return: number of built-ins
  **/
+
 int shell_num_builtins(built_s builtin[])
 {
-unsigned int i;
-
-for (i = 0; builtin[i].name != NULL; i++)
-	;
-return (i);
+	unsigned int i;
+	for (i = 0; builtin[i].name != NULL)
+		;
+			(i++);
+	return (i);
 }
